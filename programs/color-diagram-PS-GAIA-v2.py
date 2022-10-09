76e4038438d521a7289a2d4faa5c400c61e48c21
AngelGSoto <gsoto.angel@gmail.com>
2022-10-08 22:49:05 -0300

    Finally I made the right color-color diagram

--- programs/color-diagram-PS-GAIA-v2.py
+++ programs/color-diagram-PS-GAIA-v2.py
@@ -1,3 +1,4 @@
+import __future__
 import numpy as np
 import json
 import matplotlib.pyplot as plt
@@ -5,8 +6,15 @@ from  astropy.table import Table, vstack, hstack
 import pandas as pd
 from matplotlib.colors import PowerNorm
 import seaborn as sns
+from scipy.optimize import fsolve
 from pathlib import Path
 ROOT_PATH = Path("data")
+
+
+#Find the point inteception between two lines     
+def findIntersection(m, y, m1, y1, x0):
+    x = np.linspace(-10.0, 15.5, 200)
+    return fsolve(lambda x : (m*x + y) - (m1*x + y1), x0)
 
 # Planetary nebula
 tab_pn = Table.read("PN-PS-Gaia.ecsv", format="ascii.ecsv")
@@ -43,7 +51,6 @@ for cat, metadata in catalogs.items():
     bp_rp = table[metadata["BP-RP"]]
     for i, ii in zip(G_r, bp_rp):
         if (np.isfinite(i) and np.isfinite(ii)):
-            print(i, ii)
             G_r_.append(i)
             bp_rp_.append(ii)
 
@@ -66,16 +73,19 @@ alpha = [0.4, 0.6, 0.6, 0.6, 0.6, 0.6, 0.6]
 #Plotting
 lgd_kws = {'frameon': True, 'fancybox': True, 'shadow': True}
 sns.set_style('ticks')
-fig = plt.figure(figsize=(10, 8))
-ax = fig.add_subplot(111)
-ax.scatter(bp_rp_pn, G_r_pn, s=30, c = sns.xkcd_palette(["forest green"]), edgecolors= "w", zorder = 1, lw=0.5, alpha = 0.6, label = "PN")
+fig, ax = plt.subplots(figsize=(12, 10))
+plt.tick_params(axis='x', labelsize=30) 
+plt.tick_params(axis='y', labelsize=30)
+#fig = plt.figure(figsize=(10, 8))
+#ax = fig.add_subplot(111)
+ax.scatter(bp_rp_pn, G_r_pn, s=80, c = sns.xkcd_palette(["forest green"]), edgecolors= "g", zorder = 1, lw=0.5, alpha = 0.7, label = "PN")
 #ax.scatter(bp_rp_star, G_r_star, s=50, c = sns.xkcd_palette(["forest green"]), edgecolors= "k", zorder = 11, alpha = 0.6, label = "Stars")
 
 sns.kdeplot(
     bp_rp_star, G_r_star,
     ax=ax,
     bw_method='scott',
-    levels=[0.1, 0.2, 0.4, 0.5, 0.7, 0.8, 1],
+    levels=[0.05, 0.08, 0.1, 0.2, 0.4, 0.5, 0.7, 0.8, 1],
     norm=PowerNorm(0.5),
     cmap="Blues",
     zorder = 3
@@ -85,17 +95,30 @@ sns.kdeplot(
     bp_rp_, G_r_,
     ax=ax,
     bw_method='scott',
-    levels=[0.1, 0.2, 0.4, 0.5, 0.6, 0.7, 0.8, 1],
+    levels=[0.05, 0.08, 0.1, 0.2, 0.4, 0.5, 0.6, 0.7, 0.8, 1],
     norm=PowerNorm(0.5),
     cmap="Reds", zorder = 2,
      )
 #ax.scatter(bp_rp_, G_r_, s=50, c = sns.xkcd_palette(["purple"]), edgecolors= "k", zorder = 10, alpha = 0.6, label = "Other EM")
+plt.xlabel(r'$G_{BP} - G_{RP}$', fontsize=30)
+plt.ylabel(r'$G - r$', fontsize=30)
+ax.set_xlim(-1.2, 5.7)
+ax.set_ylim(-3.3, 9.2)
 
-plt.xlabel(r'$G_{BP} - G_{RP}$')
-plt.ylabel(r'$G - r$')
-#ax.set_xlim(-30.0, 390.0)
-#ax.set_ylim(-90.0, 90.0)
-ax.legend(prop={'family': 'monospace', 'size': 'x-large'}, **lgd_kws)
+# Region where are located the PNe
+result = findIntersection(1/3.6, 0.5, -10.8, 31.26, 0.0)
+
+x_new = np.linspace(-15.5, result,  200)
+x_new2 = np.linspace(result, -10.0, 200)
+x_new3 = np.linspace(-10.0, 1.1, 200)
+y = (1/3.6)*x_new + 0.5
+yy = -10.8*x_new2 + 31.26
+#Mask
+#mask = y >= result_y - 0.5
+ax.plot(x_new, y, color='k', linestyle='-')
+ax.plot(x_new2, yy , color='k', linestyle='-')
+
+ax.legend(prop={'family': 'monospace', 'size': 30}, **lgd_kws)
 #plt.gca().invert_yaxis()
 fig.savefig("Figs/color-diagram-ps-gaia-v2.pdf")
 plt.clf()
