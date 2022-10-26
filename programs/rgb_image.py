ce4d821addc3037d8fd5e0febe6a5b0754dbe388
AngelGSoto <gsoto.angel@gmail.com>
2022-10-26 16:33:00 -0300

    Adding the coloured images

---
+++ programs/rgb_image.py
@@ -0,0 +1,192 @@
+'''
+Making RGB images from PLUS .fits
+Based on original: rgb_image.py
+Autor: L. A. Gutiérrez Soto
+02/09/20
+'''
+
+from __future__ import print_function
+import aplpy
+import numpy
+import sys
+from astropy import coordinates as coord
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+import argparse
+import matplotlib.pyplot as plt
+from astropy.io import fits
+import matplotlib
+matplotlib.use("Agg")
+
+parser = argparse.ArgumentParser(
+    description="""Plot side-by-side RGB images of sources""")
+
+parser.add_argument("image_r", type=str,
+                    default="1000001-JPLUS-01485-v2_iSDSS_swp-crop",
+                    help="Name of original FITS image (section in database) in i")
+
+parser.add_argument("image_g", type=str,
+                    default="1000001-JPLUS-01485-v2_rSDSS_swp-crop",
+                    help="Name of original FITS image (section in database) in r")
+
+parser.add_argument("image_b", type=str,
+                    default="1000001-JPLUS-01485-v2_gSDSS_swp-crop",
+                    help="Name of original FITS image (section in database) in g")
+
+# parser.add_argument("--name", type=str,
+#                     default="PSP",
+#                     help="Name of the objet")
+
+parser.add_argument("--vmin_r", type=float, default=None,
+                    help="""Set minimum brightness directly - overrides minfactor - r""")
+parser.add_argument("--vmax_r", type=float, default=None,
+                    help="""Set maximum brightness directly - overrides maxfactor - r""")
+
+parser.add_argument("--vmin_g", type=float, default=None,
+                    help="""Set minimum brightness directly - overrides minfactor - g""")
+parser.add_argument("--vmax_g", type=float, default=None,
+                    help="""Set maximum brightness directly - overrides maxfactor - g""")
+
+parser.add_argument("--vmin_b", type=float, default=None,
+                    help="""Set minimum brightness directly - overrides minfactor - b""")
+parser.add_argument("--vmax_b", type=float, default=None,
+                    help="""Set maximum brightness directly - overrides maxfactor - b""")
+
+parser.add_argument("--zoom", type=float, default=None,
+                    help="""\Zoom factor to adjust size of plot box - values > 1.0 mean to zoom in""")
+
+parser.add_argument("--position", type=str,
+                    default="HYDRA-0026-000010640-position",
+                    help="Find the DS9 region")
+
+parser.add_argument("--debug", action="store_true",
+                    help="Print out verbose debugging info")
+
+
+cmd_args = parser.parse_args()
+image_r = cmd_args.image_r + ".fits"
+image_g = cmd_args.image_g + ".fits"
+image_b = cmd_args.image_b + ".fits"
+
+hdul_r = fits.open(image_r)
+instrument_r = hdul_r[0].header['BAND']
+hdul_g = fits.open(image_g)
+instrument_g = hdul_g[0].header['BAND']
+hdul_b = fits.open(image_b)
+instrument_b = hdul_b[0].header['BAND']
+
+#aplpy.make_rgb_cube(['1000001-JPLUS-01485-v2_iSDSS_swp-crop.fits', '1000001-JPLUS-01485-v2_rSDSS_swp-crop.fits',
+                     #'1000001-JPLUS-01485-v2_gSDSS_swp-crop.fits'], 'JPLUS_cube.fits')
+
+aplpy.make_rgb_cube([image_r, image_g, image_b], image_r.replace('.fits', '_cube.fits'))
+
+aplpy.make_rgb_image(image_r.replace('.fits', '_cube.fits'),
+                              image_r.replace('.fits', '_rgb.png'),
+                      vmin_r=cmd_args.vmin_r, vmax_r=cmd_args.vmax_r, vmin_g=cmd_args.vmin_g,
+                                                      vmax_g=cmd_args.vmax_g, vmin_b=cmd_args.vmin_b, vmax_b=cmd_args.vmax_b)
+
+#aplpy.make_rgb_image('JPLUS_cube.fits','JPLUS_linear.png')
+#hdul = fits.open('JPLUS_cube_2d.fits')
+# aplpy.make_rgb_image('JPLUS_cube.fits','JPLUS_rgb.png',
+#                       stretch_r='arcsinh', stretch_g='arcsinh',
+#                       stretch_b='arcsinh')
+
+
+# With the mask regions, the file may not exist
+position = cmd_args.position + ".reg"
+ra, dec = [], []
+
+try:
+    f = open(position, 'r')
+    header1 = f.readline()
+    header2 = f.readline()
+    header3 = f.readline()
+    for line in f:
+        line = line.strip()
+        columns = line.split()
+        coor = line.split("(")[-1].split("\"")[0]
+        ra1, dec1 = coor.split(",")[0:2]
+        c = SkyCoord(ra1, dec1, unit=(u.hourangle, u.deg))
+        ra.append(c.ra.hourangle)
+        dec.append(c.dec.degree)
+
+except FileNotFoundError:
+    print("File", position,
+                "not found - is not necesary now")
+    
+# Launch APLpy figure of 2D cube
+img = aplpy.FITSFigure(image_r.replace('.fits', '_cube_2d.fits')) 
+img.show_rgb(image_r.replace('.fits', '_rgb.png'))
+
+# Maybe we would like the arcsinh stretched image more?
+#img.show_rgb('ic348_color_arcsinh.png')
+
+# Modify the tick labels for precision and format
+# img.tick_labels.set_xformat('hh:mm:ss')
+# img.tick_labels.set_yformat('dd:mm')
+img.axis_labels.set_xtext('RA (J2000)')
+#img.axis_labels.hide_x()
+img.axis_labels.set_ytext('Dec (J2000)')
+img.axis_labels.set_font(size=18, weight='medium', stretch='normal', family='sans-serif', style='normal', variant='normal')
+img.axis_labels.hide()
+img.axis_labels.hide_y()
+
+img.tick_labels.set_font(size=18, weight='medium', stretch='normal', family='sans-serif', style='normal', variant='normal')
+#img.axis_labels.set_yposition('right')
+#img.tick_labels.set_yposition('right')
+#img.tick_labels.hide()
+#img.tick_labels.hide_x()  # Hide the x axis
+#img.tick_labels.hide_y()  # Hide the y axis
+# Let's add a scalebar to it
+img.add_scalebar(30.0/3600.) #20
+img.scalebar.set_label('30"')
+img.scalebar.set(color='white', linewidth=4, alpha=0.9)
+img.scalebar.set_font(size=45, weight='bold',
+                      stretch='normal', family='sans-serif',
+                      style='normal', variant='normal')
+
+#Filter names
+img.add_label(0.1, 0.9, "W" + str(instrument_b) + "+" + "W" + str(instrument_g) + "+" +  "W" + str(instrument_r), color="white",
+              horizontalalignment='left',
+              weight='bold', size=20, relative=True, zorder=1000)
+dx, dy = 0.001, -0.001
+# img.add_label(0.1+dx, 0.9+dy, instrument_b, color="black", alpha=0.6,
+#               horizontalalignment='left',
+#               bbox={"facecolor": "black", "edgecolor": "none",# "pad": 20,
+#                     "alpha": 0.5, "boxstyle": "round, pad=0.5"},
+#               weight='bold', size=55, relative=True, zorder=999)
+
+try:
+    img.show_regions(position)
+except FileNotFoundError: 
+    print('Region not found')
+# except FileNotFoundError:
+#     print("File", position,
+#                 "not found - is not necesary now")
+
+# img.show_markers(ra, dec, layer='marker_set_1', edgecolor='red',
+#                  facecolor='red', marker='o', s=10, alpha=1.)
+#img.show_markers(ra, dec , layer="marker_set_1", edgecolor="red", facecolor="none", marker="o", s=10,  alpha=0.5)
+try:
+    img.recenter(ra, dec, radius = cmd_args.zoom/3600.) #degree, for this reazon a split into 3600. #zoom ax2.recenter(ra0, dec0, 4*R0/cmd_args.zoom)
+except TypeError:
+    print("No zoom here")
+#img.show_markers(ra, dec, layer='marker', edgecolor='red', facecolor='none', marker='o', s=10, alpha=0.9, linewidths=100.)#, layer='marker_set_1', edgecolor='black', facecolor='none', s=30, alpha=0.5, linewidths=20)
+# img.scalebar.set_font(size=23, weight='bold',
+#                       stretch='normal', family='sans-serif',
+#                       style='normal', variant='normal')
+
+# We may want to lengthen the scalebar, move it to the top left,
+# and apply a physical scale
+#img.scalebar.set_corner('top left')
+# img.scalebar.set_length(20/3600.)
+# img.scalebar.set_label('20 arcsec')
+
+if cmd_args.debug:
+    print("Creating of PDF image of:", position.split('-p')[0])
+
+img.set_theme('publication')
+if image_r.endswith("_swp-crop.fits"):
+    img.save(image_r.replace('_swp-crop.fits', '-'+str(instrument_r)+str(instrument_g)+str(instrument_b)+'-RGB.png'))
+else:
+    img.save(image_r.replace('.fits', '-'+str(instrument_r)+str(instrument_g)+str(instrument_b)+'-RGB.pdf'))
--- programs/spectra-lamost.py
+++ programs/spectra-lamost.py
@@ -24,10 +24,10 @@ wl = hdudata[2]
 Flux = hdudata[0]
 
 # Emission lines
-wv_lin = [3869.76, 3967.46, 4101.74, 4340.471, 4685.71, 4711.26, 4740.120, 4861.33, 4958.92, 5006.8, 5411, 6562.82,  7005.87, 7113, 7135, 7751, 8236.79]
+wv_lin = [3869.76, 3967.46, 4101.74, 4340.471, 4685.71, 4711.26, 4740.120, 4861.33, 4958.92, 5006.8, 5411, 6562.82, 6879,  7005.87, 7113, 7135, 7751, 8236.79, 9001.27]
 #em_lin = [r"[Ne III]" + "\n" + "3869", "[Ne III] + H7" + "\n" + "3968", r"H$\delta$", r"H$\gamma$", "He II", "[Ar IV]" + "\n" + "4711", "[Ar IV]" + "\n" + "4740", r"H$\beta$", "[O III]" + "\n" + "4958", "[O III]" + "\n" + "5007", "He II" + "\n" + "5411", "[N II]", r"H$\alpha$", "[N II]", "[S II]" + "\n" + "6731", "[Ar V]" + "\n" + "7005", "?" + "\n" + "7113", "[Ar III]" + "\n" + "7135", "[Ar III]" + "\n" + "7751", "He II" + "\n" + "8236", "[S III]" + "\n" + "9069"]
 
-em_lin = [r"[Ne III] 3869", "[Ne III] + H7 3968", r"H$\delta$", r"H$\gamma$", "He II", "[Ar IV] 4711", "[Ar IV] 4740", r"H$\beta$", "[O III] 4958", "[O III] 5007", "He II 5411",  r"H$\alpha$", "[Ar V] 7005", "? 7113", "[Ar III] 7135", "[Ar III] 7751", "He II 8236"]
+em_lin = [r"[Ne III] 3869", "[Ne III] + H7 3968", r"H$\delta$", r"H$\gamma$", "He II", "[Ar IV] 4711", "[Ar IV] 4740", r"H$\beta$", "[O III] 4958", "[O III] 5007", "He II 5411",  r"H$\alpha$", "? 6879", "[Ar V] 7005", "? 7113", "[Ar III] 7135", "[Ar III] 7751", "He II 8236", "? 9001"]
 
 #m = (5000 < wl) &  (6000 > wl)
 #new_flux = Flux[m].max()
@@ -68,7 +68,7 @@ for wll in wv_lin:
 # ax.axvline(4363.21, color='k', linewidth=0.4, linestyle='--', label="[O III] 4363")
 # ax.axvline(4958.92, color='k', linewidth=0.4, linestyle='--', label="[O III] 4958")
 
-bbox_props = dict(boxstyle="round", fc="w", ec="0.78", alpha=0.6, pad=0.1)
+bbox_props = dict(boxstyle="round", fc="w", ec="0.88", alpha=0.6, pad=0.1)
 for label_, x, y in zip(em_lin, wv_lin, max_flux):
     ax.annotate(label_, (x, y), alpha=1, size=4,
                    xytext=(3.0, 5.6), textcoords='offset points', ha='right', va='bottom', rotation=90, bbox=bbox_props, zorder=100)
@@ -80,6 +80,30 @@ plt.savefig(namefile)
 ra = hdu[0].header["RA"]
 dec = hdu[0].header["DEC"]
 print("Nome file:", namefile, "=>", "RA and DEC:", ra, dec)
+
+############################################################
+#----------------------------------------------------------#
+############################################################
+fig, ax = plt.subplots(figsize=(11, 5))
+#ax.set_title(namefile)
+ax.set(xlim=[3600,9100])
+plt.ylim(ymin=-200,ymax=1500)
+ax.set(xlabel='Wavelength $(\AA)$')
+ax.set(ylabel='Flux')
+ax.plot(wl, Flux, c = "blueviolet", linewidth=0.7, zorder=5)
+for wll in wv_lin:
+    ax.axvline(wll, color='k', linewidth=0.4, alpha=0.5, linestyle='--')
+
+bbox_props = dict(boxstyle="round", fc="w", ec="0.78", alpha=0.6, pad=0.1)
+for label_, x, y in zip(em_lin, wv_lin, max_flux):
+    ax.annotate(label_, (x, y), alpha=1, size=4,
+                   xytext=(3.0, 5.6), textcoords='offset points', ha='right', va='bottom', rotation=90, bbox=bbox_props, zorder=100)
+    
+#ax.legend()
+plt.tight_layout()
+namefile0 = file_.replace(".fits", "-zoom.pdf")
+plt.savefig(namefile0)
+
 
 ############################################################
 #----------------------------------------------------------#
